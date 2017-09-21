b85a4f97f58e10f521e5591fec65ef1006616d14
Juha Lundan <juha.lundan@gmail.com>
2017-09-21 23:16:00 +0300

    Implement returning SpaCy info when calling the root with GET

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,38 @@ let express = require('express');
 let router = express.Router();
 const spacyNLP = require('spacy-nlp');
 const nlp = spacyNLP.nlp;
+let fs = require('fs');
 
-router.get('/', (req, res) => {
-    res.send({title: 'SpaCy JSON service'});
+router.get('/', (req, res, next) => {
+    try {
+        fs.readFile('/etc/spacy_info', 'utf8', function (err, fileContents) {
+            if (err) {
+                next(err);
+            }
+
+            let response = {
+                title: 'SpaCy JSON service',
+                spacyInfo: {}
+            };
+
+            let spacyInfoKeys = ["Name", "Version" ];
+
+            let lines = fileContents.split(/\r?\n/);
+
+            for (let line of lines) {
+                let splits = line.split(':');
+                if (splits.length === 2 && splits[0] && splits[1] && spacyInfoKeys.indexOf(splits[0].trim()) !== -1) {
+                    response.spacyInfo[splits[0].trim().toLowerCase()] = splits[1].trim();
+                }
+            }
+
+            response.spacyInfo['website'] = "https://spacy.io/";
+
+            res.send(response);
+        });
+    } catch (e) {
+        next(e);
+    }
 });
 
 router.post('/', (req, res, next) => {
@@ -17,7 +46,7 @@ router.post('/', (req, res, next) => {
             next(e);
         });
     } else {
-        let error = { message: "Missing field: input" };
+        let error = {message: "Missing field: input"};
         error.status = 400;
         next(error);
     }
